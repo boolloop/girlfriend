cba883d92e1c1a32346464622c55253bee13b4c4
boolloop <66819311+boolloop@users.noreply.github.com>
2021-12-24 22:08:08 +0700

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -33,8 +33,6 @@
 
         //do something later when date is reached
         if (distance < 0) {
-          location.href = "https://boolloop.github.io/happybirthdayjulie/";
-         
         }
         //seconds
       }, 0)
